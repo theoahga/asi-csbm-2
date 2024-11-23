76bb448ed689bb834ed27e1b4d98cd7895fe4ddf
Theo Clere <clere.theo@gmail.com>
2024-11-23 10:56:18 +0100

    feat :  logging microservice

---
+++ microservice-logging/src/main/java/com/theoahga/microservice_logging/config/ActiveMQConfig.java
@@ -0,0 +1,28 @@
+package com.theoahga.microservice_logging.config;
+
+import jakarta.jms.ConnectionFactory;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+
+import static jakarta.jms.Session.AUTO_ACKNOWLEDGE;
+
+@Configuration
+public class ActiveMQConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new ActiveMQConnectionFactory();
+    }
+
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setConcurrency("1-10");
+        factory.setSessionAcknowledgeMode(AUTO_ACKNOWLEDGE);
+        return factory;
+    }
+}
---
+++ microservice-logging/src/test/java/com/theoahga/microservice_logging/MicroserviceLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theoahga.microservice_logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
